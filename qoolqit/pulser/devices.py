d56b8eebe3506e5d2336d06a2614eae7db79d8a3
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2025-03-27 17:58:48 +0100

    some more device properties

--- qoolqit/pulser/devices.py
+++ qoolqit/pulser/devices.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, InitVar, asdict
 
 from pulser.devices import AnalogDevice as _AnalogDevice
 from pulser.devices import MockDevice as _MockDevice
@@ -10,14 +10,26 @@ from pulser.devices._device_datacls import BaseDevice
 @dataclass
 class Device:
 
-    _device: BaseDevice
+    device: InitVar[BaseDevice]
+    name: str | None = None
+    max_n_qubits: int | None = None
+    min_qubit_distance: float | None = None
+    max_sequence_duration: int | None = None
 
-    def __post_init__(self) -> None:
+    def __post_init__(self, device: BaseDevice) -> None:
+        self._device = device
         self.name: str = self._device.name
         self.max_n_qubits: int | None = self._device.max_atom_num
+        self.min_qubit_distance: float = self._device.min_atom_distance
+        self.max_sequence_duration: int | None = self._device.max_sequence_duration
 
     def __repr__(self) -> str:
         return self.name
+    
+    @property
+    def specs_dict(self):
+        """Device specifications."""
+        return asdict(self)
 
 
 MockDevice = Device(_MockDevice)
