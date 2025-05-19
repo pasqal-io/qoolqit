13792a2253889696add147ef6ab9ddfba44d60b4
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2025-05-19 15:38:24 +0200

    add register and waveforms previous draft

--- qoolqit/pulser/__init__.py
+++ qoolqit/pulser/__init__.py
@@ -1,8 +1,28 @@
 from __future__ import annotations
 
 from .devices import AnalogDevice, MockDevice
+from .register import Register
+from .waveforms import (
+    BlackmanWaveform,
+    CompositeWaveform,
+    ConstantWaveform,
+    CustomWaveform,
+    InterpolatedWaveform,
+    KaiserWaveform,
+    PWLWaveform,
+    RampWaveform,
+)
 
 __all__ = [
     "MockDevice",
     "AnalogDevice",
+    "Register",
+    "BlackmanWaveform",
+    "CompositeWaveform",
+    "ConstantWaveform",
+    "CustomWaveform",
+    "InterpolatedWaveform",
+    "KaiserWaveform",
+    "RampWaveform",
+    "PWLWaveform",
 ]
