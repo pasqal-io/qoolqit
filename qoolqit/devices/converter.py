859a1ba327ecace9723da4066c7e0698cb917a72
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2025-05-22 13:54:06 +0200

    converter dataclass

--- qoolqit/devices/converter.py
+++ qoolqit/devices/converter.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import InitVar, dataclass
+from dataclasses import InitVar, dataclass, field
 from math import pi
 
 from .device import Device
@@ -22,16 +22,26 @@ class UnitConverter:
 
     device: InitVar[Device]
     """Time conversion factor."""
-    time: float | None = None
+    time: float = field(init=False)
     """Energy conversion factor."""
-    energy: float | None = None
+    energy: float = field(init=False)
     """Distance conversion factor."""
-    distance: float | None = None
+    distance: float = field(init=False)
 
     def __post_init__(self, device: Device) -> None:
-
         self._device = device
+        self._c6 = self._device._c6
 
-        self.energy = self._device._max_amp or 4.0 * pi
+        # For now we just default to setting the reference energy
+        energy = self._device._max_amp or 4.0 * pi
+        self.set_energy_unit(energy)
+
+    def set_energy_unit(self, energy: float) -> None:
+        self.energy = energy
         self.time = 1000.0 / self.energy
-        self.distance = (self._device._c6 / self.energy) ** (1 / 6)
+        self.distance = (self._c6 / self.energy) ** (1 / 6)
+
+    def set_distance_unit(self, distance: float) -> None:
+        self.distance = distance
+        self.energy = self._c6 / (self.distance**6)
+        self.time = 1000.0 / self.energy
