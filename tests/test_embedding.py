58d0a19c52b52d128ec38c285a3f31d7faabbf37
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2025-06-17 12:12:30 +0200

    fix test

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -93,15 +93,16 @@ def test_custom_embedders() -> None:
 
     def some_wrong_embedding_algo(data: DataGraph, param1: float) -> float:
         return 2.0
-    
-    graph = DataGraph.random_er(5, 0.5)
 
     embedder = GraphToGraphEmbedder(some_wrong_embedding_algo, SomeConfig())
 
     with pytest.raises(TypeError):
+        graph = DataGraph.random_er(5, 0.5)
         embedder.embed(graph)
 
     embedder = MatrixToGraphEmbedder(some_wrong_embedding_algo, SomeConfig())
 
     with pytest.raises(TypeError):
-        embedder.embed(graph)
+        matrix = np.random.rand(5, 5)
+        matrix = matrix + matrix.T
+        embedder.embed(matrix)
