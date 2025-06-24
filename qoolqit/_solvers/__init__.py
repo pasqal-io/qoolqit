5de53f72bb3bd0d2793da67ca29ae64043da99b2
mlahariya <40852060+mlahariya@users.noreply.github.com>
2025-06-24 14:55:12 +0200

    Update init

--- qoolqit/_solvers/__init__.py
+++ qoolqit/_solvers/__init__.py
@@ -1,54 +1,27 @@
 from __future__ import annotations
 
-import os
+from importlib import import_module
 
-from qoolqit._solvers.types import DeviceType
-
-from .backends.base_backend import (
+from .backends import *
+from .data import (
     BackendConfig,
-    BaseBackend,
     BaseJob,
+    CompilationError,
+    ExecutionError,
     JobId,
     QuantumProgram,
     Result,
 )
-from .backends.get_backend import get_backend
-from .backends.local_backends import BaseLocalBackend, QutipBackend
-from .backends.remote_backends import (
-    RemoteEmuFREEBackend,
-    RemoteEmuMPSBackend,
-    RemoteEmuTNBackend,
-    RemoteJob,
-    RemoteQPUBackend,
-)
-from .data import BackendType, CompilationError, ExecutionError
+from .types import BackendType, DeviceType
 
 __all__ = [
     "BackendConfig",
-    "BaseBackend",
     "BackendType",
     "BaseJob",
-    "BaseLocalBackend",
-    "BaseRemoteBackend",
     "CompilationError",
     "ExecutionError",
     "JobId",
-    "QuantumProgram",
-    "QutipBackend",
-    "RemoteQPUBackend",
-    "RemoteEmuMPSBackend",
-    "RemoteEmuTNBackend",
-    "RemoteEmuFREEBackend",
     "Result",
-    "get_backend",
-    "RemoteJob",
     "DeviceType",
+    "QuantumProgram",
 ]
-
-if os.name == "posix":
-    from .backends.local_backends import EmuMPSBackend, EmuSVBackend
-
-    __all__ += [
-        "EmuMPSBackend",
-        "EmuSVBackend",
-    ]
