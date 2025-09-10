eb5e780a16c5784fc2fb556dfa96a99a7b0a45e3
Stefano Grava <s.grava-ext@pasqal.com>
2025-09-10 18:18:39 +0200

    example class

---
+++ qoolqit/pulserbackend.py
@@ -0,0 +1,51 @@
+from pulser.backend import Backend, EmulatorBackend, EmulationConfig, Results
+from pulser.backend.remote import RemoteBackend, RemoteResults
+from pulser.backend.qpu import QPUBackend
+from pulser_pasqal import PasqalCloud
+from pulser_simulation import QutipBackendV2
+
+# import pulser.backends as backends
+from typing import Type
+from qoolqit import QuantumProgram
+
+
+class PulserBackend:
+    """Class to run Sequence/Quantum program on multiple backends."""
+
+    def __init__(
+        self,
+        program: QuantumProgram,
+        backend_type: Type[Backend] = QutipBackendV2,
+        backend_config: EmulationConfig | None = None,
+        connection: PasqalCloud | None = None,
+    ):
+        if issubclass(backend_type, EmulatorBackend):
+            pass
+        elif issubclass(backend_type, RemoteBackend):
+            pass
+        elif issubclass(backend_type, QPUBackend):
+            pass
+        else:
+            raise TypeError(f"{backend_type.__name__} is not a valid backend.")
+
+        self.backend_type = backend_type
+        self.backend_config = backend_config
+        self.connection = connection
+
+    def run(self) -> Results | RemoteResults:
+        """Run a QuantumProgram and return the results."""
+
+        compiled_seq = self.program._compiled_sequence
+
+        if issubclass(self.backend_type, EmulatorBackend):
+            backend = self.backend_type(compiled_seq, config=self.backend_config)
+            return backend.run()
+        elif issubclass(self.backend_type, RemoteBackend):
+            backend = self.backend_type(
+                compiled_seq, connection=self.connection, config=self.backend_config
+            )
+            # RemoteBackend requires kwarg `job_params` which is then ignored
+            return backend.run(job_params=[{"nruns": 100}], wait=True)
+        elif issubclass(self.backend_type, QPUBackend):
+            backend = self.backend_type(compiled_seq, connection=self.connection)
+            return backend.run()
