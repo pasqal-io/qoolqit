d130f7baf739acf9e1504965d469dfc4bc792248
Stefano Grava <s.grava-ext@pasqal.com>
2025-11-21 11:40:06 +0100

    update test

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
-import sys
-
 from qoolqit import __version__ as init_version
 
 
 def test_qoolqit_version() -> None:
     """Check that the qoolqit version is the same for different ways to query it."""
-    if sys.version_info >= (3, 11):
-        import tomllib
+    try:
+        import tomllib  # Python v3.11+
 
         with open("pyproject.toml", "rb") as file:
             pyproject_version = tomllib.load(file)["project"]["version"]
 
         assert init_version == pyproject_version
+    except ModuleNotFoundError:
+        pass
