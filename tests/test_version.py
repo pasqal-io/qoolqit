f81dec2b2616cbce372d11606aeea57bab33ff72
Stefano Grava <s.grava-ext@pasqal.com>
2025-11-21 11:23:30 +0100

    test version only for python >3.11

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
-import tomllib
+import sys
 
 from qoolqit import __version__ as init_version
 
 
 def test_qoolqit_version() -> None:
     """Check that the qoolqit version is the same for different ways to query it."""
-    with open("pyproject.toml", "rb") as file:
-        pyproject_version = tomllib.load(file)["project"]["version"]
+    if sys.version_info >= (3, 11):
+        import tomllib
 
-    assert init_version == pyproject_version
+        with open("pyproject.toml", "rb") as file:
+            pyproject_version = tomllib.load(file)["project"]["version"]
+
+        assert init_version == pyproject_version
