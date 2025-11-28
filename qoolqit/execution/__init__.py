235bfff78a2e20776daff2fc2d449332ae21ba07
Stefano Grava <s.grava-ext@pasqal.com>
2025-11-28 17:24:09 +0100

    add observables

--- qoolqit/execution/__init__.py
+++ qoolqit/execution/__init__.py
@@ -2,7 +2,19 @@ from __future__ import annotations
 
 from emu_mps import MPSBackend, MPSConfig
 from emu_sv import SVBackend, SVConfig
-from pulser.backend import EmulationConfig
+from pulser.backend import (
+    BitStrings,
+    CorrelationMatrix,
+    EmulationConfig,
+    Energy,
+    EnergySecondMoment,
+    EnergyVariance,
+    Expectation,
+    Fidelity,
+    Occupation,
+    Results,
+    StateResult,
+)
 from pulser.backend.remote import RemoteResults
 from pulser_pasqal import EmuFreeBackendV2, EmuMPSBackend
 from pulser_simulation import QutipBackendV2, QutipConfig
@@ -12,19 +24,29 @@ from qoolqit.execution.sequence_compiler import SequenceCompiler
 from qoolqit.execution.utils import CompilerProfile
 
 __all__ = [
-    "MPSBackend",
-    "MPSConfig",
-    "SVBackend",
-    "SVConfig",
-    "EmulationConfig",
-    "RemoteResults",
-    "EmuFreeBackendV2",
-    "EmuMPSBackend",
-    "QutipBackendV2",
-    "QutipConfig",
     "SequenceCompiler",
     "CompilerProfile",
     "LocalEmulator",
     "RemoteEmulator",
     "QPU",
+    "QutipBackendV2",
+    "QutipConfig",
+    "MPSBackend",
+    "MPSConfig",
+    "SVBackend",
+    "SVConfig",
+    "EmuFreeBackendV2",
+    "EmuMPSBackend",
+    "EmulationConfig",
+    "BitStrings",
+    "CorrelationMatrix",
+    "Energy",
+    "EnergySecondMoment",
+    "EnergyVariance",
+    "Expectation",
+    "Fidelity",
+    "Occupation",
+    "StateResult",
+    "Results",
+    "RemoteResults",
 ]
