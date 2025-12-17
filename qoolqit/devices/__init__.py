24b79c86643e3187e7f449dc639ef95705680a67
Stefano Grava <s.grava-ext@pasqal.com>
2025-12-17 12:54:11 +0100

    add available def devices to namespace

--- qoolqit/devices/__init__.py
+++ qoolqit/devices/__init__.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
-from .device import (
-    AnalogDevice,
-    Device,
-    DigitalAnalogDevice,
-    MockDevice,
-)
+from .device import AnalogDevice, Device, DigitalAnalogDevice, MockDevice, available_default_devices
 
 __all__ = [
     "MockDevice",
     "AnalogDevice",
     "DigitalAnalogDevice",
     "Device",
+    "available_default_devices",
 ]
