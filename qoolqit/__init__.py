ccf543e28ea1a78d7ff17659fe99563e7b102b94
Stefano Grava <42062939+sgrava@users.noreply.github.com>
2026-01-12 10:29:04 +0100

    add blackman waveform (#150)
    * add blackman
    
    * return float
    
    * move blackman to waveforms
    
    * add Blackman to init
    
    * update docs
    
    * add test
    
    * better docstring
    
    * sign error in docs

--- qoolqit/__init__.py
+++ qoolqit/__init__.py
@@ -20,7 +20,7 @@ from qoolqit.execution import CompilerProfile, SequenceCompiler
 from qoolqit.graphs import DataGraph
 from qoolqit.program import QuantumProgram, store_package_version_metadata
 from qoolqit.register import Register
-from qoolqit.waveforms import Constant, Delay, Interpolated, PiecewiseLinear, Ramp, Sin
+from qoolqit.waveforms import Blackman, Constant, Delay, Interpolated, PiecewiseLinear, Ramp, Sin
 
 __all__ = [
     "DataGraph",
@@ -28,6 +28,7 @@ __all__ = [
     "InteractionEmbeddingConfig",
     "SpringLayoutConfig",
     "SpringLayoutEmbedder",
+    "Blackman",
     "Constant",
     "Delay",
     "Interpolated",
