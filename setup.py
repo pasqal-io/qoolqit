58495d9b0a317f05fa125a1325ef022651fc584f
Stefano Grava <s.grava-ext@pasqal.com>
2026-01-12 18:09:01 +0100

    update pyproject since pulser[torch] does not install at CEA

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="qoolqit",
     version="0.3.3",
-    install_requires=["pulser~=1.6.3", "torch~=2.9", "networkx~=3.4"],
+    install_requires=["pulser~=1.6.3", "torch", "networkx~=3.4"],
     packages=find_packages(),
 )
