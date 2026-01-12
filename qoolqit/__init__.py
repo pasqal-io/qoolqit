050a442772c0e72b207dc510b318c7e68e6fba45
Stefano Grava <42062939+sgrava@users.noreply.github.com>
2026-01-12 18:45:47 +0100

    optional emulators (#145)
    * optional emulators
    
    * nbmake in default env
    
    * extras dependencies
    
    * remove test repetition
    
    * take backend type from pulser
    
    * use new backendtype in tutturials
    
    * update docstring
    
    * update execution docs
    
    * add info to setup.py
    
    * update documentation
    
    * store metadata only for pulser 1.6.3
    
    * import as pulser_sequence
    
    * fix licence in pyproject
    
    * bump pulser dep
    
    * Antoine comments
    
    * fix setup.py
    
    * fix setup.py forgot comma
    
    * update pyproject since pulser[torch] does not install at CEA
    
    * messed up as it was working just fine before

--- qoolqit/__init__.py
+++ qoolqit/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import pulser.sequence as pulser_sequence
+
 from qoolqit.devices import (
     AnalogDevice,
     Device,
@@ -18,7 +20,7 @@ from qoolqit.embedding import (
 )
 from qoolqit.execution import CompilerProfile, SequenceCompiler
 from qoolqit.graphs import DataGraph
-from qoolqit.program import QuantumProgram, store_package_version_metadata
+from qoolqit.program import QuantumProgram
 from qoolqit.register import Register
 from qoolqit.waveforms import Blackman, Constant, Delay, Interpolated, PiecewiseLinear, Ramp, Sin
 
@@ -48,6 +50,6 @@ __all__ = [
 ]
 
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
-store_package_version_metadata("qoolqit", __version__)
+pulser_sequence.store_package_version_metadata("qoolqit", __version__)
