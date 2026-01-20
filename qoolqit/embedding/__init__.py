581e599aa172e782da25f18eaa4b8478c76a8b7a
Stefano Grava <s.grava-ext@pasqal.com>
2026-01-20 16:41:37 +0100

    expose blade embedder

--- qoolqit/embedding/__init__.py
+++ qoolqit/embedding/__init__.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
 from .algorithms import InteractionEmbeddingConfig, SpringLayoutConfig
+from .algorithms.blade_embedding.blade_embedding import BladeEmbeddingConfig
 from .base_embedder import BaseEmbedder, EmbeddingConfig
 from .graph_embedder import GraphToGraphEmbedder, SpringLayoutEmbedder
-from .matrix_embedder import InteractionEmbedder, MatrixToGraphEmbedder
+from .matrix_embedder import BladeEmbedder, InteractionEmbedder, MatrixToGraphEmbedder
 
 __all__ = [
+    "SpringLayoutConfig",
     "SpringLayoutEmbedder",
+    "InteractionEmbeddingConfig",
     "InteractionEmbedder",
+    "BladeEmbeddingConfig",
+    "BladeEmbedder",
 ]
