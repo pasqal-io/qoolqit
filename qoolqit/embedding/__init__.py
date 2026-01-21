330a8303ad5c7a155cf927c83488a779e9dbd6f5
Stefano Grava <s.grava-ext@pasqal.com>
2026-01-21 14:04:25 +0100

    fix embedding imports

--- qoolqit/embedding/__init__.py
+++ qoolqit/embedding/__init__.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
 
 from .algorithms import BladeEmbeddingConfig, InteractionEmbeddingConfig, SpringLayoutConfig
-from .graph_embedder import SpringLayoutEmbedder
-from .matrix_embedder import BladeEmbedder, InteractionEmbedder
+from .base_embedder import BaseEmbedder, EmbeddingConfig
+from .graph_embedder import GraphToGraphEmbedder, SpringLayoutEmbedder
+from .matrix_embedder import BladeEmbedder, InteractionEmbedder, MatrixToGraphEmbedder
 
 __all__ = [
-    "SpringLayoutConfig",
     "SpringLayoutEmbedder",
+    "BaseEmbedder",
+    "EmbeddingConfig",
+    "GraphToGraphEmbedder",
+    "MatrixToGraphEmbedder",
     "InteractionEmbeddingConfig",
     "InteractionEmbedder",
+    "SpringLayoutConfig",
+    "SpringLayoutEmbedder",
     "BladeEmbeddingConfig",
     "BladeEmbedder",
 ]
