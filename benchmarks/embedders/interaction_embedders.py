65c8e8d8f8fede613b878bd93b90cd1cbfc487f9
Stefano Grava <s.grava-ext@pasqal.com>
2026-02-16 16:42:13 +0100

    update benchmark

--- benchmarks/embedders/interaction_embedders.py
+++ benchmarks/embedders/interaction_embedders.py
@@ -1,20 +1,26 @@
-import numpy as np
-from qoolqit import InteractionEmbeddingConfig, BladeConfig
+from __future__ import annotations
 
-from qoolqit.embedding.algorithms.interaction_embedding import interaction_embedding
-from qoolqit.embedding.algorithms.blade.blade import blade
+import numpy as np
 from sklearn.manifold import smacof
 
+from qoolqit import BladeConfig, InteractionEmbeddingConfig
+from qoolqit.embedding.algorithms.blade.blade import blade
+from qoolqit.embedding.algorithms.interaction_embedding import interaction_embedding
 
-def interaction_matrix(pos):
+
+def interaction_matrix(pos: np.ndarray) -> np.ndarray:
     n = len(pos)
     interactions = np.zeros((n, n))
     for i in range(n):
-        for j in range(i+1, n):
-            interaction = np.linalg.norm(pos[i]-pos[j])**(-6)
+        for j in range(i + 1, n):
+            interaction = np.linalg.norm(pos[i] - pos[j]) ** (-6)
             interactions[i, j] = interaction
             interactions[j, i] = interaction
     return interactions
+
+
+def stress_from_positions(target: np.ndarray, pos: np.ndarray) -> np.floating:
+    return np.linalg.norm(target - interaction_matrix(pos))
 
 
 # qubo = np.random.random((5, 5))
@@ -22,27 +28,24 @@ def interaction_matrix(pos):
 # qubo /= qubo.max()
 # np.fill_diagonal(qubo, 0.0)
 
-qubo = interaction_matrix(np.random.random((4, 2)))
+
+qubo = interaction_matrix(np.random.random((6, 2)))
 qubo /= qubo.max()
 print(qubo)
 
-qubo_for_smacof = qubo
+qubo_for_smacof = np.zeros_like(qubo)
 mask = qubo > 0.0
-qubo_for_smacof[mask] = qubo[mask]**(-1/6)
+qubo_for_smacof[mask] = qubo[mask] ** (-1 / 6)
 
-
-ie_config = InteractionEmbeddingConfig()
+ie_config = InteractionEmbeddingConfig(tol=1e-9)
 b_config = BladeConfig()
 
-res_smacof = smacof(qubo_for_smacof)
-pos_smacof = res_smacof[0]
+res_smacof = smacof(qubo_for_smacof, eps=1e-7, max_iter=1000)
+pos_smacof = np.array(res_smacof[0])
 pos_ie = interaction_embedding(qubo, **ie_config.dict())
 pos_blade = blade(qubo, **b_config.dict())
 
 
-
-
-
-print(interaction_matrix(pos_smacof))
-print(interaction_matrix(pos_ie))
-print(interaction_matrix(pos_blade))
+print(stress_from_positions(qubo, pos_smacof))
+print(stress_from_positions(qubo, pos_ie))
+print(stress_from_positions(qubo, pos_blade))
