Add feature to regulate priorities + fix filter differences for scaling + other enhancements, plot improvements, fixes and tests

qoolqit/embedding/algorithms/blade/_dist_constraints_forces.py
@@ -28,12 +28,12 @@ def compute_min_dist_constraint_forces(
 def compute_max_dist_constraint_forces(
     *,
     positions: np.ndarray,
-    max_dist: float | None,
+    max_radius: float | None,
 ) -> Force:
     distances_from_center = np.linalg.norm(positions, axis=1)
     max_distances_to_walk = (
-        np.maximum(0, distances_from_center - max_dist)
-        if max_dist is not None
+        np.maximum(0, distances_from_center - max_radius)
+        if max_radius is not None
         else np.full_like(distances_from_center, 0)
     )
     max_weights = max_distances_to_walk

qoolqit/embedding/algorithms/blade/_distances_constraints_calculator.py
@@ -13,7 +13,7 @@ def compute_best_scaling_for_qubo(
     target_interactions: np.ndarray,
     embedded_interactions: np.ndarray,
     filter_differences: bool = True,
-    plot: bool = False,
+    draw_differences: bool = False,
 ) -> Any:
     embedded_interactions_triu = embedded_interactions[
         np.triu_indices_from(embedded_interactions, k=1)
@@ -26,22 +26,22 @@ def compute_best_scaling_for_qubo(
     percentile = np.percentile(differences, percent)
 
     difference_ceiling = np.maximum(0.0, percentile)
-    limited_differences = np.minimum(differences, difference_ceiling)
-
-    if plot:
-        plot_differences(target_interactions, differences)
 
     if filter_differences:
-        # when no visible differences, use the input value to avoid rounding issues
-        embedded_interactions_triu = np.where(
-            differences == limited_differences,
-            embedded_interactions_triu,
-            target_interactions_triu + limited_differences,
+        weights = np.where(
+            differences > difference_ceiling,
+            difference_ceiling / differences,
+            1.0,
         )
+    else:
+        weights = np.ones_like(differences)
+
+    if draw_differences:
+        plot_differences(differences)
 
     best_scaling = (
-        np.sum(embedded_interactions_triu**2)
-        / np.sum(embedded_interactions_triu * target_interactions_triu)
+        np.sum(weights * embedded_interactions_triu**2)
+        / np.sum(weights * embedded_interactions_triu * target_interactions_triu)
     ) ** (1 / 6)
 
     assert not np.isnan(best_scaling)
@@ -52,7 +52,7 @@ def compute_best_scaling_for_qubo(
 
 
 def compute_best_scaling_for_pos(
-    target_interactions: np.ndarray, positions: np.ndarray, plot: bool = False
+    target_interactions: np.ndarray, positions: np.ndarray, draw_differences: bool = False
 ) -> Any:
     distance_matrix = distance_matrix_from_positions(positions)
 
@@ -62,7 +62,9 @@ def compute_best_scaling_for_pos(
     current_weights = np.triu(current_weights, k=1)
 
     return compute_best_scaling_for_qubo(
-        target_interactions=target_interactions, embedded_interactions=current_weights, plot=plot
+        target_interactions=target_interactions,
+        embedded_interactions=current_weights,
+        draw_differences=draw_differences,
     )
 
 
@@ -83,13 +85,15 @@ class DistancesConstraintsCalculator:
             self.current_min = None
 
     def compute_scaling_min_max(
-        self, positions: np.ndarray, step_cursor: float, plot: bool = False
+        self, positions: np.ndarray, step_cursor: float, draw_differences: bool = False
     ) -> tuple[float, Optional[float], Optional[float]]:
         """Step_cursor is between 0 (start) and 1 (end)."""
         assert 0 <= step_cursor <= 1
 
         scaling_factor = compute_best_scaling_for_pos(
-            target_interactions=self.target_interactions, positions=positions, plot=plot
+            target_interactions=self.target_interactions,
+            positions=positions,
+            draw_differences=draw_differences,
         )
 
         if self.final_ratio is None:

qoolqit/embedding/algorithms/blade/_force.py
@@ -2,11 +2,66 @@ from __future__ import annotations
 
 import dataclasses
 import logging
+from functools import cached_property
 from typing import Any
 
 import numpy as np
+from numpy.typing import ArrayLike
 
 logger = logging.getLogger(__name__)
+
+
+def configured_increasing_func(
+    x: ArrayLike, *, middle_value: float = 0.5, stiffness: float = 1.0
+) -> np.ndarray:
+    """Increasing function from [0, 1] to [0, 1]."""
+
+    c = 1 / middle_value - 1
+    x = np.asarray(x, dtype=float)
+    return np.power(x, stiffness) / (np.power(x, stiffness) + c * np.power(1 - x, stiffness))
+
+
+def modulate_cursor(
+    *,
+    strong_cursor: ArrayLike,
+    weak_cursor: ArrayLike,
+    center_value: float = 0.25,
+    stiffness: float = 1.0,
+) -> np.ndarray:
+    strong_cursor = np.asarray(strong_cursor, dtype=float)
+    weak_cursor = np.asarray(weak_cursor, dtype=float)
+
+    weak_coeff_multiplier = configured_increasing_func(
+        weak_cursor, middle_value=1 / (1 / center_value - 1), stiffness=stiffness
+    )
+
+    out_mid = (
+        strong_cursor * weak_coeff_multiplier / (1 + strong_cursor * (weak_coeff_multiplier - 1))
+    )
+
+    out = np.where(
+        (strong_cursor == 1) & (weak_cursor == 0),
+        0,
+        out_mid,
+    )
+
+    return out
+
+
+def clip_modulate_cursor(
+    *,
+    strong_cursor: ArrayLike,
+    weak_cursor: ArrayLike,
+    center_value: float = 0.25,
+    stiffness: float = 1.0,
+) -> np.ndarray:
+    value = modulate_cursor(
+        strong_cursor=strong_cursor,
+        weak_cursor=weak_cursor,
+        center_value=center_value,
+        stiffness=stiffness,
+    )
+    return np.clip(value, 0, 1)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -17,17 +72,59 @@ class Force:
     def __post_init__(self) -> None:
         assert self.weighted_vectors.shape[:-1] == self.distances_to_walk.shape
         assert not np.any(np.isnan(self.weighted_vectors))
+        self.maximum_temperatures
 
     def get_nb_dims(self) -> int:
         return len(self.weighted_vectors.shape)
 
-    def get_temperature(self) -> Any | int:
-        vector_weights = np.linalg.norm(self.weighted_vectors, axis=self.get_nb_dims() - 1)
-        logger.debug(f"{vector_weights=}")
-        # remove warning output
+    def _fit_to_dims(self, a: np.ndarray) -> np.ndarray:
+        return a.reshape((*a.shape, *((1,) * (self.get_nb_dims() - len(a.shape)))))
+
+    def regulated(
+        self,
+        regulation_cursor: float = 0,
+    ) -> "Force":
+        min_temperature = np.min(self.maximum_temperatures)
+        temperature_ratios = self.maximum_temperatures / min_temperature
+        weight_ratios = self.vector_weights / np.max(self.vector_weights)
+        used_cursors = clip_modulate_cursor(
+            strong_cursor=regulation_cursor, weak_cursor=weight_ratios
+        )
+        regulated_temperature_ratios = temperature_ratios ** (1 - used_cursors)
+        regulated_weighted_vectors = (
+            self.weighted_vectors
+            * self._fit_to_dims(temperature_ratios)
+            / self._fit_to_dims(regulated_temperature_ratios)
+        )
+        regulated_weighted_vectors[self.weighted_vectors == 0] = 0
+        return Force(
+            weighted_vectors=regulated_weighted_vectors,
+            distances_to_walk=self.distances_to_walk,
+        )
+
+    @cached_property
+    def vector_weights(self) -> np.ndarray:
+        return np.linalg.norm(self.weighted_vectors, axis=self.get_nb_dims() - 1)
+
+    @cached_property
+    def maximum_temperatures(self) -> np.ndarray:
+        vector_weights = self.vector_weights
         with np.errstate(divide="ignore", invalid="ignore"):
             maximum_temperatures = self.distances_to_walk / vector_weights
         maximum_temperatures[vector_weights == 0] = np.inf
+
+        assert np.all(
+            (
+                maximum_temperatures[np.triu_indices_from(maximum_temperatures, k=1)]
+                if maximum_temperatures.ndim == 2
+                else maximum_temperatures
+            )
+            > 0
+        )
+        return maximum_temperatures
+
+    def get_temperature(self) -> Any | int:
+        maximum_temperatures = self.maximum_temperatures
         logger.debug(f"{maximum_temperatures=}")
 
         min_temperature = np.min(maximum_temperatures)