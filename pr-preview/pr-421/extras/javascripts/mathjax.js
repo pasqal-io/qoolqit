a416d87027c19daada74f44ece796d574208f91a
alessandro-santini <alessandro-santini@users.noreply.github.com>
2026-07-31 09:23:47 +0000

    Deploy preview for PR 421 🛫

--- pr-preview/pr-421/extras/javascripts/mathjax.js
+++ pr-preview/pr-421/extras/javascripts/mathjax.js
@@ -1,19 +1,27 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"], ["$", "$"]],
-    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    inlineMath: [
+      ["\\(", "\\)"],
+      ["$`", "`$"],
+      ["$", "$"]
+    ],
+    displayMath: [
+      ["\\[", "\\]"],
+      ["$$`", "`$$"],
+      ["$$", "$$"]
+    ],
     processEscapes: true,
     processEnvironments: true
   },
   options: {
     ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex|jp-RenderedMarkdown|jp-RenderedHTMLCommon"
+    processHtmlClass:
+      "arithmatex|jp-RenderedMarkdown|jp-RenderedHTMLCommon"
   }
 };
 
 document$.subscribe(() => {
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
-})
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise().catch(error => console.error(error));
+});
