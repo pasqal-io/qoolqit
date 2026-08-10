3ba054f9dbe333f8189921aa1a95ce20b1bd1608
Stefano Grava <s.grava-ext@pasqal.com>
2026-08-10 18:06:40 +0200

    remove comments top file

--- qoolqit/graphs/data_graph.py
+++ qoolqit/graphs/data_graph.py
@@ -1,8 +1,3 @@
-# TODO:
-# - refactor this to reuse common methods in constructors
-# - refactor using rescale_coords method
-
-
 from __future__ import annotations
 
 from collections.abc import Iterable
