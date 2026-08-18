0e6cce3a64c14ca1b8dc8327c6fa5326be4a5f02
Stefano Grava <42062939+sgrava@users.noreply.github.com>
2026-08-18 14:29:17 +0200

    remove _to_pulser workaround in InterpolatedWaveform (#454)

--- qoolqit/execution/compilation_functions.py
+++ qoolqit/execution/compilation_functions.py
@@ -13,7 +13,6 @@ from qoolqit.devices import Device
 from qoolqit.drive import DetuningMapModulator, Drive, Waveform
 from qoolqit.exceptions import CompilationError
 from qoolqit.register import Register
-from qoolqit.waveforms import InterpolatedWaveform
 
 
 class CompilerProfile(Enum):
@@ -58,14 +57,6 @@ class WaveformConverter:
     def convert(self, waveform: Waveform) -> ParamObj | PulserWaveform:
         """Convert a QoolQit waveform into a equivalent Pulser waveform."""
         pulser_duration = self._pulser_duration(waveform)
-
-        # Interpolated to be converted differently because of round off issue:
-        # see https://github.com/pasqal-io/qoolqit/issues/288
-        # see https://github.com/pasqal-io/Pulser/issues/1051
-        if isinstance(waveform, InterpolatedWaveform):
-            wf = waveform._to_pulser(duration=pulser_duration, energy_factor=self._energy)
-            return wf
-
         return waveform._to_pulser(duration=pulser_duration) * self._energy
 
 
